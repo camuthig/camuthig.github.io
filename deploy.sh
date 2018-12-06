080c4b3302b8c3797285f93ee011caa99154bdc8
Chris Muthig <camuthig@gmail.com>
2018-12-05 19:01:15 -0800

    Update deploy script per Hugo docs

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,25 @@
-#! /bin/sh
+#!/bin/bash
 
-echo "Deploying site..."
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Build the project.
 hugo
 
-cd public && git add --all && git commit -m "Generating site" && git push origin master
+# Go To Public folder
+cd public
 
-echo "Done!"
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
