d01a6f37668f230fbbc9c46ad2f6c2dc99ace97d
Chris Muthig <camuthig@gmail.com>
2019-04-03 17:33:15 +1030

    Update the about me page a bit

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,13 +10,18 @@
 @section('body')
     <h1>About</h1>
 
-    <img src="/assets/img/about.png"
+    <img src="/assets/img/profile.jpeg"
         alt="About image"
         class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p class="mb-6">
+        My name is Chris Muthig, and I am a software developer with a passion for helping others get more out of technology.
+        I have had the opportunity to work in a number of different domains, and I'm always looking for new tools I can help build
+        to solve problems people face every day.
+    </p>
+    <p class="mb-6">
+        I have worked in a number of different languages over the years and believe that programming languages are just a tool to solve complex problems.
+        You'll often see me experimenting with new languages and trying out new development paradigms to see if they can better
+        solve certain problems I may come across.
+    </p>
 @endsection
